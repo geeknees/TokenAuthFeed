deb30007096d3dcfc08ee4bc11e52bc1fb0564c0
Masumi Kawasaki <geeknees@gmail.com>
2022-01-30 10:17:15 +0900

    add wallet connection

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,28 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Provider, chain, defaultL2Chains } from "wagmi";
+import { InjectedConnector } from "wagmi/connectors/injected";
+
+const chains = defaultL2Chains;
+
+const connectors = ({ chainId }) => {
+  const rpcUrl =
+    chains.find((x) => x.id === chainId)?.rpcUrls?.[0] ??
+    chain.mainnet.rpcUrls[0];
+  return [
+    new InjectedConnector({
+      chains: chains,
+      options: { shimDisconnect: true }
+    })
+  ];
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Provider autoConnect connectors={connectors}>
+      <Component {...pageProps} />{" "}
+    </Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
